230d2afbc4efb76f65658bb62da0d52787c6c0e3
Jarosław Kubiak <jaroslaw.kubiak@huuugegames.com>
2024-02-19 12:04:14 +0100

    finish section 2: ngIf, ngFor, ngStyle and ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,13 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styles: [`
+  .online {
+    color: white;
+  }
+  `]
 })
 export class ServerComponent {
   serverId: number = 10;
-  serverStatus: string = 'offline';
+  serverStatus: string = '';
   test = false;
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getBgColor() {
+    return this.serverStatus === 'online' ? '#00ff00' : '#ff0000';
+  }
+
+
+
   getServerStatus() {
-    return this.serverStatus
+    return this.serverStatus;
   }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,16 +8,21 @@ import { Component } from '@angular/core';
 export class ServersComponent {
   serverCreationStatus = 'No server was created!';
   serverName = '';
+  serverWasCreated = false;
+  servers = ['TestServer 1', 'TestServer 2', 'TestServer 3', 'TestServer 4'];
+  displayDetails = false;
+  displayClick = [];
 
   onCreateServer() {
     this.serverCreationStatus = `Server ${this.serverName} was created!`;
+    this.serverWasCreated = true;
+    this.servers.push(this.serverName);
   }
 
   allowNewServer = false;
   serverBtnCaption = 'Turn On';
 
   onTurnOn() {
-    console.log('on turn on');
     this.allowNewServer = !this.allowNewServer;
     this.allowNewServer
       ? (this.serverBtnCaption = 'Turn Off')
@@ -26,5 +31,11 @@ export class ServersComponent {
 
   onUpdateSererName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  onDisplayDetails() {
+    this.displayClick.push(new Date());
+    console.log(this.displayClick);
+    return (this.displayDetails = !this.displayDetails);
   }
 }
