6fb51bf54b6b9b3fcb957152ade34bf8f33d18e5
Jarosław Kubiak <jaroslaw.kubiak@huuugegames.com>
2024-02-25 19:11:21 +0100

    section 6 - active header menu, display recipe and details

--- recipe-app/src/app/header/header.component.ts
+++ recipe-app/src/app/header/header.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   collapsed = true;
+  @Output()
+  featureSelected = new EventEmitter<string>();
+
+  onSelect(feature: string) {
+    this.featureSelected.emit(feature);
+  }
 }
--- recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,16 +7,18 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
-  recipes: Recipe[] = [
-    new Recipe(
-      'Pizza',
-      'nduja, jalapenio, chorizo',
-      'https://dacostantino.pl/wp-content/uploads/2022/01/IMG_20230120_141616_1-01-scaled.jpeg'
-    ),
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+  recipesList: Recipe[] = [
+    new Recipe('Pizza', 'nduja, jalapenio, chorizo', 'https://dacostantino.pl/wp-content/uploads/2022/01/IMG_20230120_141616_1-01-scaled.jpeg'),
     new Recipe(
       'Pasta',
       'tomato, bazyl, parmegiano',
-      'https://www.allrecipes.com/thmb/5SdUVhHTMs-rta5sOblJESXThEE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/11691-tomato-and-garlic-pasta-ddmfs-3x4-1-bf607984a23541f4ad936b33b22c9074.jpg'
+      'https://www.allrecipes.com/thmb/5SdUVhHTMs-rta5sOblJESXThEE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/11691-tomato-and-garlic-pasta-ddmfs-3x4-1-bf607984a23541f4ad936b33b22c9074.jpg',
     ),
   ];
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
