8fcd56bda122b61c207287369db7ea7ef8fd9646
Jarosław Kubiak <jaroslaw.kubiak@huuugegames.com>
2024-03-10 19:58:58 +0100

    add routing to recipe app

--- recipe-app/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ recipe-app/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -8,9 +10,20 @@ import { RecipeService } from '../recipe.service';
   styleUrl: './recipe-detail.component.css',
 })
 export class RecipeDetailComponent {
-  @Input() recipe: Recipe;
+  recipe: Recipe;
+  id: number;
 
-  constructor(private recipeService: RecipeService) {}
+  constructor(
+    private recipeService: RecipeService,
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.id = +params['id'];
+      this.recipe = this.recipeService.getRecipe(this.id);
+    });
+  }
 
   addToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
