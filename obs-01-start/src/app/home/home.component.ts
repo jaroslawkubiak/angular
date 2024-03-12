d3c93a78422e6ed822aa375756612bb8d3c7a3f7
Jarosław Kubiak <jaroslaw.kubiak@huuugegames.com>
2024-03-12 23:33:56 +0100

    finish section 13 - observables

--- obs-01-start/src/app/home/home.component.ts
+++ obs-01-start/src/app/home/home.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { Subscription, interval } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
+import { Observable } from 'rxjs-compat';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstObsSubscription: Subscription;
+  constructor() {}
 
   ngOnInit() {
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // });
+
+    const customIntervalObservable = Observable.create(observer => {
+      let counter = 0;
+      setInterval(() => {
+        observer.next(counter);
+        if (counter === 5) {
+          observer.complete();
+        }
+        if (counter > 3) {
+          observer.error(new Error('Count is greater than 3!'));
+        }
+        counter++;
+      }, 1000);
+    });
+
+    this.firstObsSubscription = customIntervalObservable
+      .pipe(
+        filter((data: number) => data % 2 === 0),
+        map((data: number) => {
+          return 'Round: ' + (data + 1);
+        }),
+      )
+      .subscribe(
+        data => {
+          console.log(data);
+        },
+        error => {
+          console.error(error);
+          console.log(error.message);
+        },
+        () => {
+          console.log(`complete!`);
+        },
+      );
   }
 
+  ngOnDestroy() {
+    // console.log(this.firstObsSubscription);
+    this.firstObsSubscription.unsubscribe();
+  }
 }
--- obs-01-start/src/app/user/user.component.ts
+++ obs-01-start/src/app/user/user.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
   id: number;
+  defaultActivate = true;
 
-  constructor(private route: ActivatedRoute) {
-  }
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService,
+  ) {}
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.id = +params.id;
     });
+  }
+
+  onActivate() {
+    // this.userService.activatedEmitter.emit(true);
+    this.userService.activatedEmitter.next(this.defaultActivate);
+    this.defaultActivate = !this.defaultActivate;
   }
 }
