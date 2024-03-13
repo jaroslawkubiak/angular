f704bc01c3c4078af9a5936ac041f82138be1afa
Jarosław Kubiak <jaroslaw.kubiak@huuugegames.com>
2024-03-13 22:09:17 +0100

    finish section 14 - refactor using subscription

--- recipe-app/src/app/recipes/recipe.service.ts
+++ recipe-app/src/app/recipes/recipe.service.ts
@@ -1,12 +1,10 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
-
   private recipes: Recipe[] = [
     new Recipe('Pizza', 'Very hot and spicy pizza', 'https://dacostantino.pl/wp-content/uploads/2022/01/IMG_20230120_141616_1-01-scaled.jpeg', [
       new Ingredient('nduja', 4),
