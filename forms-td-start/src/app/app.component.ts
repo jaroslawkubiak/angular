5e378cb95747afa60cfbdf03564269b4f4ce9309
Jarosław Kubiak <jaroslaw.kubiak@huuugegames.com>
2024-03-14 23:50:27 +0100

    section 15 - template driven approach

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
+  defaultUser = 'Jarek';
+  defaultEmail = 'jaroslaw@gmail.com';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: '',
+  };
+  submitted = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.signupForm.form.patchValue({ userData: { username: suggestedName } });
   }
+
+  onSubmit() {
+    console.log(this.signupForm.value);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
+  }
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  //   console.log('Submitted!');
+  // }
 }
