1585436b1e4f1fbce81a3f86edd73d24b7ea756b
Jarosław Kubiak <jaroslaw.kubiak@huuugegames.com>
2024-03-16 23:22:55 +0100

    section 15 - finish assignment with td aproach

--- forms-td-task/src/app/app.component.ts
+++ forms-td-task/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('form') signupForm: NgForm;
+  public valid: boolean = false;
+  subscriptions = ['Basic', 'Advanced', 'Pro'];
+  defaultSubscription = 'Advanced';
 
+  userForm = {
+    submitted: false,
+    username: '',
+    email: '',
+    subscription: '',
+  };
+
+  onSubmit() {
+    this.userForm.submitted = true;
+    this.userForm.username = this.signupForm.form.value.username;
+    this.userForm.email = this.signupForm.form.value.email;
+    this.userForm.subscription = this.signupForm.form.value.subscription;
+  }
 }
