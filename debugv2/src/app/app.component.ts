49d14032a9f3f91ddc1f34181ed9e71cb4122745
Jarosław Kubiak <jaroslaw.kubiak@huuugegames.com>
2025-01-15 16:30:33 +0100

    finish section 2

--- debugv2/src/app/app.component.ts
+++ debugv2/src/app/app.component.ts
@@ -14,7 +14,7 @@ import { TasksComponent } from "./tasks/tasks.component";
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId?: string;
+  selectedUserId = 'u1';
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId);
