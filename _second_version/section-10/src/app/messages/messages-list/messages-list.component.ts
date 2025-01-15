8271e46ddfe3891ec2e65bbfbf3a39900c3b2b14
Jarosław Kubiak <jaroslaw.kubiak@huuugegames.com>
2025-01-15 19:39:01 +0100

    simplyfy code using async pipe

--- _second_version/section-10/src/app/messages/messages-list/messages-list.component.ts
+++ _second_version/section-10/src/app/messages/messages-list/messages-list.component.ts
@@ -7,31 +7,37 @@ import {
   OnInit,
 } from '@angular/core';
 import { MessagesService } from '../messages.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-messages-list',
   standalone: true,
+  imports: [AsyncPipe],
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MessagesListComponent implements OnInit {
+export class MessagesListComponent {
   private messagesService = inject(MessagesService);
 
-  private changeDetRef = inject(ChangeDetectorRef);
-  private destroyRef = inject(DestroyRef);
-  messages: string[] = [];
-  ngOnInit() {
-    const subscription = this.messagesService.messages$
-      .subscribe((messages) => {
-        this.messages = messages;
-        this.changeDetRef.markForCheck();
-      });
+  // private changeDetRef = inject(ChangeDetectorRef);
+  // private destroyRef = inject(DestroyRef);
+  // messages: string[] = [];
 
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe();
-    });
-  }
+  messagesList$ = this.messagesService.messages$;
+
+
+  // ngOnInit() {
+  //   const subscription = this.messagesService.messages$
+  //     .subscribe((messages) => {
+  //       this.messages = messages;
+  //       this.changeDetRef.markForCheck();
+  //     });
+
+  //   this.destroyRef.onDestroy(() => {
+  //     subscription.unsubscribe();
+  //   });
+  // }
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
