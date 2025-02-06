9eabeaecae41bef83046271df59aab2e4b16ece1
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2025-02-06 22:54:26 +0100

    finish reactive form approach

--- _second_version/section-13-forms/src/app/auth/login-reactive/login.component.ts
+++ _second_version/section-13-forms/src/app/auth/login-reactive/login.component.ts
@@ -1,9 +1,93 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { debounceTime, of } from 'rxjs';
+
+function mustContainQuestionMark(control: AbstractControl) {
+  if (control.value.includes('?')) {
+    return null;
+  }
+
+  return { doesNotContainQuestionMark: true };
+}
+
+function emailIsUnique(control: AbstractControl) {
+  if (control.value !== 'test@test.pl') {
+    return of(null);
+  }
+
+  return of({ emailIsNotUnique: true });
+}
 
 @Component({
   selector: 'app-login-reactive',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginReactiveComponent {}
+export class LoginReactiveComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.email, Validators.required],
+      asyncValidators: [emailIsUnique],
+    }),
+    password: new FormControl('', {
+      validators: [
+        Validators.minLength(6),
+        Validators.required,
+        mustContainQuestionMark,
+      ],
+    }),
+  });
+
+  ngOnInit() {
+    const savedForm = window.localStorage.getItem('saved-login-form');
+    if (savedForm) {
+      const loadedForm = JSON.parse(savedForm);
+      this.form.patchValue({
+        email: loadedForm.email,
+      });
+    }
+
+    const subscription = this.form.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe({
+        next: (value) => {
+          window.localStorage.setItem(
+            'saved-login-form',
+            JSON.stringify({ email: value.email })
+          );
+        },
+      });
+    this.destroyRef.onDestroy(() => subscription.unsubscribe);
+  }
+
+  get emailIsInvalid() {
+    return (
+      this.form.controls.email.touched &&
+      this.form.controls.email.invalid &&
+      this.form.controls.email.dirty
+    );
+  }
+
+  get passwordIsInvalid() {
+    return (
+      this.form.controls.password.touched &&
+      this.form.controls.password.invalid &&
+      this.form.controls.password.dirty
+    );
+  }
+
+  onSubmit() {
+    const enteredEmail = this.form.value.email;
+    const enteredPassword = this.form.value.password;
+  }
+}
