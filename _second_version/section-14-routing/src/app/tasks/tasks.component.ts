267c98521e7ca810efe11177c5b7c548beb69300
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2025-02-16 22:37:08 +0100

    finish section 14

--- _second_version/section-14-routing/src/app/tasks/tasks.component.ts
+++ _second_version/section-14-routing/src/app/tasks/tasks.component.ts
@@ -1,22 +1,32 @@
 import { Component, computed, inject, input } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
-import { Task } from './task/task.model';
 import { TasksService } from './tasks.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent],
+  imports: [TaskComponent, RouterLink],
 })
 export class TasksComponent {
   userId = input.required<string>();
+  order = input<'asc' | 'desc'>('desc');
 
   private tasksService = inject(TasksService);
 
   userTasks = computed(() =>
-    this.tasksService.allTasks().filter((task) => task.userId === this.userId())
+    this.tasksService
+      .allTasks()
+      .filter((task) => task.userId === this.userId())
+      .sort((a, b) => {
+        if (this.order() === 'asc') {
+          return a.dueDate.localeCompare(b.dueDate);
+        } else {
+          return b.dueDate.localeCompare(a.dueDate);
+        }
+      })
   );
 }
